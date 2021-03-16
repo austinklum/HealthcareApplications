c08f39797cdaf607d52f889c3aa689f0c7a149e7
Austin Klum <klumaustin@gmail.com>
2021-03-16 01:17:38 -0500

    Added user context

--- HealthcareApplications/HealthcareApplications/Models/UserModels/User.cs
+++ HealthcareApplications/HealthcareApplications/Models/UserModels/User.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace HealthcareApplications.Models.UserModels
 {
-    public class UserModel
+    public class User
     {
         public int Id { get; set; }
         public string Username { get; set; }
