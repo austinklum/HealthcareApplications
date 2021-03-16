a626ea049b0f1ddf11964b916b8598b57befc6b2
Austin Klum <klumaustin@gmail.com>
2021-03-16 01:13:08 -0500

    Created usermodel

---
+++ HealthcareApplications/HealthcareApplications/Models/UserModels/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthcareApplications.Models.UserModels
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+        public int AccountStatus { get; set; }
+        public int SecurityQuestionIndex1 { get; set; }
+        public string SecurityQuestionResponse1 { get; set; }
+        public int SecurityQuestionIndex2 { get; set; }
+        public string SecurityQuestionResponse2 { get; set; }
+        public int SecurityQuestionIndex3 { get; set; }
+        public string SecurityQuestionResponse3 { get; set; }
+
+    }
+}
