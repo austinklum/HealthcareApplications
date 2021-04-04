6539c25be9df408924b4e37dd61d1c99d429ffd0
Austin Klum <klumaustin@gmail.com>
2021-04-03 20:05:02 -0500

    Added DisplayName to Patient

--- HealthcareApplications/HealthcareApplications/Models/UserModels/Patient.cs
+++ HealthcareApplications/HealthcareApplications/Models/UserModels/Patient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,18 +9,23 @@ namespace HealthcareApplications.Models
 {
     public class Patient
     {
-
-        #region Properties
+        [DisplayName("Patient Name")]
         public String Name { get; set; }
+        [DisplayName("Patient ID")]
         public int Id { get; set; }
+        [DisplayName("Date Of Birth")]
         public DateTime DateOfBirth { get; set; }
+        [DisplayName("Address")]
         public String Address { get; set; }
+        [DisplayName("Physician ID")]
         public int PhysicianId { get; set; }
         [NotMapped]
+        [DisplayName("Physician")]
         public Physician Physician { get; set; }
         [NotMapped]
+        [DisplayName("Prescription IDs")]
         public List<int> PrescriptionIds { get; set; }
+        [DisplayName("User ID")]
         public int UserId { get; set; }
-        #endregion
     }
 }
